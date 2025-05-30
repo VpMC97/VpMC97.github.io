c6fef1a3e792c35882f1f81c739b50998417eeb5
VpMC97 <vpmendezche49@gmail.com>
2025-05-30 08:35:54 -0600

    commit proof 1

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react-swc'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: 'https://vpmc97.github.io'
+  base: 'https://vpmc97.github.io/vpmc97.github.io/',
 })
